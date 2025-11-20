5d9943364a18f89b14e941d76dcdb14479b0d65f
Munge-flip <ggonzales2@ssct.edu.ph>
2025-11-20 09:07:37 +0800

    commit

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -15,7 +15,7 @@ class ServiceController extends Controller
     {
         $serviceData = $request->all();
         if (isset($serviceData['id'])) {
-            $update = service::where("id", $serviceData['id'])->update($serviceData);
+            $update = service::where('id', $serviceData['id'])->update($serviceData);
             if ($update) {
                 echo "updated<br>";
                 echo "<a href=" . route('Service.view') . ">View</a>";
@@ -24,33 +24,33 @@ class ServiceController extends Controller
             unset($serviceData['id']);
             $saveData = service::insert($serviceData);
             if ($saveData) {
-                echo "saved<br>";
+                echo "Saved<br>";
                 echo "<a href=" . route('Service.view') . ">View</a>";
             } else {
-                echo "error<br>";
+                echo "error";
             }
         }
     }
     public function view()
     {
         $serviceData = service::all();
-        return view("view", compact("serviceData"));
+        return view('view', compact("serviceData"));
     }
     public function update(Request $request)
     {
         $id = $request->id;
         $serviceData = service::find($id);
-        return view("add", compact("serviceData"));
+        return view('add', compact('serviceData'));
     }
     public function destroy(Request $request)
     {
         $id = $request->id;
-        $delete = service::where("id", $id)->delete($id);
-        if ($delete) {
-            echo "saved<br>";
+        $destroy = service::where('id', $id)->delete($id);
+        if ($destroy) {
+            echo "deleted<br>";
             echo "<a href=" . route('Service.view') . ">View</a>";
         } else {
-            echo "error<br>";
+            echo "error";
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,10 @@ use App\Models\service;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\UserController;
 
-Route::get("/service", [ServiceController::class, "create"])->name("Service.create");
-Route::get("/store", [ServiceController::class, "store"])->name("Service.store");
-Route::get("/view", [ServiceController::class, "view"])->name("Service.view");
-Route::get("/update", [ServiceController::class, "update"])->name("Service.update");
-Route::get("/destroy", [ServiceController::class, "destroy"])->name("Service.destroy");
+Route::prefix('/service')->name('Service.')->group(function() {
+    Route::get('/', [ServiceController::class, 'create'])->name('create');
+    Route::get('/store', [ServiceController::class, 'store'])->name('store');
+    Route::get('/view', [ServiceController::class, 'view'])->name('view');
+    Route::get('/update', [ServiceController::class, 'update'])->name('update');
+    Route::get('/destroy', [ServiceController::class, 'destroy'])->name('destroy');
+});
